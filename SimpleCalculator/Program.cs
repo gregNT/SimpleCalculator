e34b808125817e8d3ad8ac2b911954df1115a594
Grzegorz Kolusz <grzegorzkolusz@gmail.com>
2019-12-30 13:38:12 +0100

    Create SimpleCalc class stub

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -6,6 +6,25 @@ using System.Threading.Tasks;
 
 namespace SimpleCalculator
 {
+    public class SimpleCalc
+    {
+        // auto-properties
+        public decimal a { get; set; }
+        public decimal b { get; set; }
+        public string operation { get; set; }
+
+        // methods
+        public void GetUserInput()
+        {
+
+        }
+
+        public decimal Evaluate()
+        {
+            return 0m;
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
