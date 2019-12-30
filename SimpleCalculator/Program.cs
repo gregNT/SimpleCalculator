31e170c684558b89c9e951a47e0eb5b75ad79fa5
Grzegorz Kolusz <grzegorzkolusz@gmail.com>
2019-12-30 15:20:46 +0100

    Write basic user input gathering

--- SimpleCalculator/Program.cs
+++ SimpleCalculator/Program.cs
@@ -8,16 +8,53 @@ namespace SimpleCalculator
 {
     public class SimpleCalc
     {
-        // auto-properties
-        public decimal a { get; set; }
-        public decimal b { get; set; }
+        // properties
+        decimal _a;
+        decimal _b;
+
         public string operation { get; set; }
 
         // methods
+        public void SetA() {
+            Console.WriteLine("Enter a (decimal): ");
+            while (!decimal.TryParse(Console.ReadLine(), out _a))
+                Console.WriteLine("The value must be of decimal type, try again: ");
+        }
+
+        public void SetB()
+        {
+            Console.WriteLine("Enter b (decimal): ");
+            while (!decimal.TryParse(Console.ReadLine(), out _b))
+                Console.WriteLine("The value must be of decimal type, try again: ");
+        }             
+        
         public void GetUserInput()
         {
+            SetA();
+            SetB();
+            /*
+            do
+            {
+                
+                isNumeric = 
+                if (isNumeric)
+                    a = val;
+                else
+                    Console.WriteLine("Error! Please specify the correct numeric value.");
+            } while (!isNumeric);
 
+            do
+            {
+                Console.WriteLine("Specify the second input (b): ");
+                isNumeric = decimal.TryParse(Console.ReadLine(), out val);
+                if (isNumeric)
+                    b = val;
+                else
+                    Console.WriteLine("Error! Please specify the correct numeric value.");
+            } while (!isNumeric);
+            */
         }
+        
 
         public decimal Evaluate()
         {
