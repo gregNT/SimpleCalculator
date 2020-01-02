9cc9e8664abb488bfe24e1db5ecba735de84fbd9
Grzegorz Kolusz <grzegorzkolusz@gmail.com>
2020-01-02 13:34:29 +0100

    Add empty class for InputConverter unit tests

---
+++ SimpleCalculator.Test.Unit/InputConverterTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SimpleCalculator.Test.Unit
+{
+    [TestClass]
+    public class InputConverterTest
+    {
+        [TestMethod]
+        public void ConvertsValidStringInputIntoDouble()
+        {
+
+        }
+
+        [TestMethod]
+        public void FailsToConvertInvalidStringInputIntoDouble()
+        {
+        }
+    }
+}
