42d316940e968ae6f74b0d7d4f087c9ddda04d4f
Grzegorz Kolusz <grzegorzkolusz@gmail.com>
2020-01-02 13:25:03 +0100

    Move Evaluate method to CalculatorEngine class

--- SimpleCalculator/CalculatorEngine.cs
+++ SimpleCalculator/CalculatorEngine.cs
@@ -8,5 +8,31 @@ namespace SimpleCalculator
 {
     class CalculatorEngine
     {
+        public decimal Evaluate(string argOperation, decimal argFirstNumber, decimal argSecondNumber)
+        {
+            decimal result;
+            switch (argOperation)
+            {
+                case "+":
+                case "add":
+                    result = argFirstNumber + argSecondNumber;
+                    break;
+                case "-":
+                case "subtract":
+                    result = argFirstNumber - argSecondNumber;
+                    break;
+                case "*":
+                case "multiply":
+                    result = argFirstNumber * argSecondNumber;
+                    break;
+                case "/":
+                case "divide":
+                    result = argFirstNumber / argSecondNumber;
+                    break;
+                default:
+                    throw new InvalidOperationException("Invalid operator specified.");
+            }
+            return result;
+        }
     }
 }
